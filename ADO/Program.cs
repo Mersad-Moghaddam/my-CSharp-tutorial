aec31a7bc5c69cccc836115280f3c529acb4435a
Mersad <nickmersad81@gmail.com>
2025-06-24 22:52:06 +0330

    ADO

---
+++ ADO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CRUD
+{
+    class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        
+        
+    }   
+}
--- TPL/Program.cs
+++ TPL/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+namespace Thread
+{
+    internal class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    class Program
+    {
+        static List<Student> GetStudentData() => Enumerable.Range(1, 10).Select(
+            i => new Student()
+            {
+                ID = i,
+                Name = $"Student-{i}"
+            }
+        ).ToList();
+
+
+        static async Task<int> ProcessStudentAsync(Student student)
+        {
+            await Task.Delay(100);
+            Console.WriteLine($"Processing Data for {student.Name}");
+            return student.ID * 2;
+
+        }
+
+        static async Task Main()
+        {
+            Console.Clear();
+            
+            var students = GetStudentData();
+            var results = await Task.WhenAll(students.Select(ProcessStudentAsync));
+            Console.WriteLine($"Total Process Data : {results.Sum()}");
+            
+        }
+    }
+}
