ad04f38ae0019335bc2cc748bdf48d3a86a3900f
Mersad <nickmersad81@gmail.com>
2025-06-24 21:40:59 +0330

    File Read and Write in C#

---
+++ File/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+// using Newtonsoft.Json;
+class Program
+{
+    static void Main()
+    {
+        // Console.WriteLine($"Enter The text you want to Save : ");
+
+        // string text = Console.ReadLine();
+        // ;
+        // File.WriteAllText("ex.txt", text);
+        // Console.WriteLine("File Write");
+        // string readText = File.ReadAllText("ex.txt");
+        // Console.WriteLine($"{readText}");
+        // Json
+
+        var person = { Name = "Mersad", Age = 23, Address = "Mashhad" };
+        File.WriteAllText("person_data.json", JsonConvert.SerializeObject(person));
+        Console.WriteLine($"Person Object Serialize into File");
+
+        var deserializePerson = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText("person_data.json"));
+
+        Console.WriteLine($"Deserialize Person : {deserializePerson.Name}, {deserializePerson.Age}, {deserializePerson.Address}");
+        
+        
+        
+        
+        
+    }
+}
